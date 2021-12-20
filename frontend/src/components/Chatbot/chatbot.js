6f0148e5a2865ff087695a18ffb9516a63da4328
kArtikirk <khairnar.kartiki@gmail.com>
2021-12-20 18:38:48 +0530

    Frontend fully functional alongwith chatbot

--- frontend/src/components/Chatbot/chatbot.js
+++ frontend/src/components/Chatbot/chatbot.js
@@ -8,7 +8,7 @@ export default function Chatbot() {
     const steps = [
         {
             id: "Greet",
-            message: "Hello, Welcome to our shop",
+            message: "Hello, Welcome to Grocery Mart",
             trigger: "Done",
         },
         {
@@ -29,20 +29,72 @@ export default function Chatbot() {
         {
             id: "issues",
             options: [
-                { value: "React", label: "React", trigger: "React",},
-                { value: "Angular", label: "Angular", trigger: "Angular" },
+                { value: "Login", label: "Login", trigger: "Login",},
+                { value: "Product", label: "Product", trigger: "Product" },
+                { value: "Cart", label: "Cart", trigger: "Cart" },
+                { value: "Other", label: "Other", trigger: "Other" },
             ],
         },
         {
-            id: "React",
-            message: "Thanks for letting your React issue, Our team will resolve your issue ASAP",
+            id: "Login",
+            message: "Please let us know your issue...",
+            trigger: 'input1',
+        },
+        {
+            
+            id: "input1",
+            user: true,
+            trigger: 'output1',
+        },
+        {
+            id: "output1",
+            message: "Thank You for reporting your issue,  Our team  will resolve your issue ASAP",
             end: true,
         },
         {
-            id: "Angular",
-            message: "Thanks for letting your Angular issue, Our team will resolve your issue ASAP",
+            id: "Product",
+            message: "Please let us know your issue...",
+            trigger: 'input2',
+        },
+        {
+            
+            id: "input2",
+            user: true,
+            trigger: 'output2',
+        },
+        {
+            id: "output2",
+            message: "Thank You for reporting your issue,  Our team  will resolve your issue ASAP",
             end: true,
         },
+        {
+            id: "Cart",
+            message: "Please let us know your issue...",
+            trigger: 'input3',
+        },
+        {
+            
+            id: "input3",
+            user: true,
+            trigger: 'output3',
+        },
+        {
+            id: "output3",
+            message: "Thank You for reporting your issue,  Our team  will resolve your issue ASAP",
+            end: true,
+        },
+        {
+            id: "Other",
+            message: "Please drop a mail to: grocery_mart _support@abc.com",
+            trigger: 'end',
+        },
+        
+        {
+            id: "end",
+            message: "Thank You !",
+            end: true,
+        },
+       
     ];
     
     return (
--- frontend/src/components/pages/Home extras/Footer.jsx
+++ frontend/src/components/pages/Home extras/Footer.jsx
@@ -1,27 +1,46 @@
 import React from 'react'
-import { Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import '../../inc/CategoryMenu.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import Chatbot from '../../Chatbot/chatbot'
 export default function Footer() {
     return (<>
         <div>
-                <section className="footer">
-                    <div className="span1">&copy; 2021 Company, Inc</div>
-                    <div>
+            <section className="footer">
+                <div className="span1">&copy; 2021 Company, Inc</div>
+                <div>
                     <ul>
                         {/* <li>
                             <Link className='link' to='/'>Home</Link>
                         </li> */}
-                        <li>
-                            
-                            <Link  className='link' to='/chatbot'>
-                                ChatBot
-                            </Link>
+
+
+                        <li style={{ width: "auto" }}>
+
+                            <Link to='/chatbot' class="nav-link active" data-bs-toggle="modal" data-bs-target="#Chatbot">
+                                <i class="fas fa-robot" style={{ fontSize: "40px" }}></i> </Link>
                         </li>
+                        <div class="modal fade" id="Chatbot" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div >
+                                        <div >
+
+                                            <Chatbot />
+
+
+                                        </div>
+
+                                    </div>
+
+
+
+                                </div>
+                            </div>
+                        </div>
                     </ul>
-                    </div>
-                </section>
+                </div>
+            </section>
         </div>
 
     </>
